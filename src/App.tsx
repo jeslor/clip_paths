d50ab4aa2f2795eb43937c3beb5734ac9248b53d
twani <jbestius@gmail.com>
2024-08-27 14:20:49 +0200

    add animations on hover

--- src/App.tsx
+++ src/App.tsx
@@ -5,36 +5,46 @@ import './App.css'
 function App() {
 
 
+  useEffect(() => {
+    const texts:any = document.querySelectorAll('h2')
+    texts.forEach((eachText:HTMLElement) => {
+      const singleText:any = SplitType.create(eachText,)
+      singleText.chars.forEach((char:any, i:number) => {
+        char.style.transitionDelay = `${i * 0.05}s`
+      });
 
-    // const text:any = SplitType.create('h2.split', )
-    // text.chars.forEach((char:any, i:number) => {
-    //   char.style.transitionDelay = `${i * 0.05}s`
-    //   char.style.transform = `translateY(${Math.random() > 0.5 ? 100 : -100}%)`
-    // })
+   
+  })
+    // return () => {
+    //   text.r()
+    // }
+  },[])
 
 
   
 
   return (
     <main className='w-screen h-screen flex overflow-hidden'>
-      <div className=" h-full overflow-hidden w-1/2 relative" > 
+      <section className='flex w-full'>
+      <div className={`h-full overflow-hidden w-1/2 relative animateClip`} > 
         <div className="absolute h-full w-full flex flex-col justify-between bg-[rgba(0,0,0,0.4)] text-slate-200 z-[2]">
-          <p className="flex items-center absolute left-6 top-[50%] [writing-mode:vertical-lr] translate-y-[-50%] uppercase font-medium">share project</p>
-          <p className="flex items-center absolute right-6 top-[50%] [writing-mode:vertical-lr] translate-y-[-50%] uppercase font-medium">view project</p>
-          <h2 className='split exo2 font-bold text-[4rem] tracking-widest w-fit mx-auto'>Design</h2>
-          <h2 className='split exo2  bottom-8 text-[4rem] tracking-widest w-fit ml-auto mr-4'>#987</h2>
+          <p className="flex items-center absolute left-6 top-[50%] [writing-mode:vertical-lr] translate-y-[-50%] uppercase font-medium text-[0.75rem]">share project</p>
+          <p className="flex items-center absolute right-6 top-[50%] [writing-mode:vertical-lr] translate-y-[-50%] uppercase font-medium text-[0.75rem]">view project</p>
+          <h2 className='split exo2 font-bold text-[4rem] w-fit mx-auto flex justify-around '>Design</h2>
+          <h2 className='transSplint exo2  bottom-8 text-[4rem] w-fit ml-auto mr-4'>#987</h2>
         </div>
         <img className=" h-full w-full object-cover absolute z-[1]" src="/jeslor.webp" alt="" />
       </div>
-      <div className=" h-full overflow-hidden w-1/2 relative">
+      <div className={`h-full overflow-hidden w-1/2 relative animateClip`}>
       <div className="absolute h-full w-full flex flex-col justify-between bg-[rgba(0,0,0,0.4)] text-slate-200 z-[2]">
-          <p className="flex items-center absolute left-8 top-[50%] [writing-mode:vertical-lr] translate-y-[-50%] uppercase font-medium">share project</p>
-          <p className="flex items-center absolute right-8 top-[50%] [writing-mode:vertical-lr] translate-y-[-50%] uppercase font-medium">view project</p>
-          <h2 className='split exo2 font-bold text-[4rem] tracking-widest w-fit mx-auto'>Design</h2>
-          <h2 className='split exo2  bottom-8 text-[4rem] tracking-widest w-fit ml-auto mr-4'>#987</h2>
+          <p className="flex items-center absolute left-8 top-[50%] [writing-mode:vertical-lr] translate-y-[-50%] uppercase font-medium text-[0.75rem]">share project</p>
+          <p className="flex items-center absolute right-8 top-[50%] [writing-mode:vertical-lr] translate-y-[-50%] uppercase font-medium text-[0.75rem]">view project</p>
+          <h2 className='split exo2 font-bold text-[4rem]  w-fit mx-auto'>Design</h2>
+          <h2 className='transSplint exo2  bottom-8 text-[4rem] tracking-widest w-fit ml-auto mr-4'>#987</h2>
         </div>
       <img className="object-cover h-full w-full" src="/landScape.webp" alt="" />
       </div>
+    </section>
     </main>
   )
 }
